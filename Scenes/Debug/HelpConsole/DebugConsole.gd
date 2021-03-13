e52154393ea6b3cb6610a5ac9984992171550824
Emilio Puga <ea.pugaavalos@ugto.mx>
2021-03-12 23:48:22 -0600

    Clean code debug console

--- Scenes/Debug/HelpConsole/DebugConsole.gd
+++ Scenes/Debug/HelpConsole/DebugConsole.gd
@@ -7,10 +7,12 @@ func _ready() -> void:
 	DebugEvents.connect("LogSubmited", self, "submit_text")
 
 func submit_text(color:Color, node_name: String, text:String) -> void:
-	var bbcodeText: String = "[color=#" + color.to_html(false) + "]"
-	bbcodeText += "[" + node_name + "]:[/color] "
-	bbcodeText += text
-	ConsoleLog.bbcode_text += bbcodeText + "\n"
+	ConsoleLog.newline()
+	ConsoleLog.push_color(color)
+	ConsoleLog.append_bbcode("[" + node_name + "]: ")
+	ConsoleLog.pop()
+	
+	ConsoleLog.append_bbcode(text)
 
 
 func _on_ToggleButton_pressed():
