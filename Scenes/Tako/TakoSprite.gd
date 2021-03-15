53327599d911c2606e82eb05d2edc8b1693693e7
Emilio Puga <ea.pugaavalos@ugto.mx>
2021-03-15 16:08:29 -0600

    Added outline shader to TakoSprite

--- Scenes/Tako/TakoSprite.gd
+++ Scenes/Tako/TakoSprite.gd
@@ -1,3 +1,12 @@
 extends Sprite
 
 onready var animationPlayer = $AnimationPlayer
+var shaderOutline = preload("res://Source/Shaders/Outline.tres")
+
+func outline(show = false):
+	if show:
+		material = shaderOutline
+		z_index = 5
+	else:
+		z_index = 0
+		material = null
