2655401efe1432f8005ed5f25000d9e3a4ebc2b6
Emilio Puga <ea.pugaavalos@ugto.mx>
2021-03-15 21:53:28 -0600

    New class weighted random, you can add items to it with a certain chance, and it'll return a random one taking into account the weights

---
+++ Source/WeightedRandom.gd
@@ -0,0 +1,28 @@
+class_name WeightedRandom
+
+var _total_weight = 0
+var _prob_table: Array
+
+func _init(_items: Array) -> void:
+	for item in _items :
+		item = item as Dictionary
+		assert(item.has_all(["item", "weight"]), "Error items")
+		add_entry(item.item, item.weight)
+	calc_probability()
+
+func add_entry(item, weight: float):
+	_total_weight += weight
+	_prob_table.append({"item": item, "weight": weight, "accumulated": _total_weight, "probability": 0.0})
+
+
+func calc_probability():
+	for item in _prob_table:
+		item.probability = item.weight / _total_weight
+
+
+func random_pick():
+	var roll: float = rand_range(0.0, _total_weight)
+	for item in _prob_table:
+		if item.accumulated > roll:
+			return item.item
+	return null
