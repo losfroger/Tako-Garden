72698ed715e5c7cc7b87642e66809a7e2f584d31
Emilio Puga <ea.pugaavalos@ugto.mx>
2021-03-15 21:55:59 -0600

    Idle now uses the weighted random class to choose between wander, idle and conversation states

--- Scenes/Tako/States/Idle.gd
+++ Scenes/Tako/States/Idle.gd
@@ -6,16 +6,25 @@ onready var timer = $Timer
 enum STATES {
 	WANDER = 0,
 	IDLE = 1,
+	CONV = 2,
 }
+
+var probStates = [
+	{"item": STATES.WANDER, "weight": 2.0},
+	{"item": STATES.IDLE, "weight": 2.0},
+	{"item": STATES.CONV, "weight": 0.1},
+	]
 
 onready var avoid: GSAIAvoidCollisions
 
+var probClass: WeightedRandom
 
-func _ready():
-	timer.connect("timeout", self, "timer_end")
+func _ready() -> void:
+	probClass = WeightedRandom.new(probStates)
 
 
 func enter(_msg := {}) -> void:
+	tako.add_to_group("Available_tako")
 	avoid = GSAIAvoidCollisions.new(tako.agent, tako.proximity_takos)
 	DebugEvents.console_print(tako.logColor, owner.name, "Idle")
 
@@ -28,15 +37,19 @@ func physics_update(delta: float) -> void:
 	tako.agent._apply_steering(tako._acceleration, delta)
 
 func exit() -> void:
+	tako.remove_from_group("Available_tako")
 	timer.stop()
 
 
-func timer_end() -> void:
-	var random_state = randi() % 2
+func _on_timer_end() -> void:
+	var random_state = probClass.random_pick()
 	match random_state:
 		STATES.WANDER:
-			state_machine.transition_to("Wander")
+			state_machine.transition_to("Move")
 		STATES.IDLE:
 			DebugEvents.console_print(tako.logColor, owner.name, "Staying in idle")
 			timer.wait_time = rand_range(0.4, 5)
 			timer.start()
+		STATES.CONV:
+			DebugEvents.console_print(tako.logColor, owner.name, "Start conv")
+			state_machine.transition_to("StartConv")
