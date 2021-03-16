2a932694b8550ec5dab524f773214045781aeada
Emilio Puga <ea.pugaavalos@ugto.mx>
2021-03-15 21:52:38 -0600

    TakoSprite sets values to the shader on ready

--- Scenes/Tako/TakoSprite.gd
+++ Scenes/Tako/TakoSprite.gd
@@ -3,6 +3,9 @@ extends Sprite
 onready var animationPlayer = $AnimationPlayer
 var shaderOutline = preload("res://Source/Shaders/Outline.tres")
 
+func _ready() -> void:
+	shaderOutline.set_shader_param("line_thickness", 10)
+
 func outline(show = false):
 	if show:
 		material = shaderOutline
