fe67d666ac7dce6031bc70342f1a26bdf8627391
Emilio Puga <ea.pugaavalos@ugto.mx>
2021-03-15 23:00:47 -0600

    Added animation tree to takoSprite

--- Scenes/Tako/TakoSprite.gd
+++ Scenes/Tako/TakoSprite.gd
@@ -1,10 +1,17 @@
 extends Sprite
 
-onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+
+var animation_state_machine: AnimationNodeStateMachinePlayback
 var shaderOutline = preload("res://Source/Shaders/Outline.tres")
 
 func _ready() -> void:
 	shaderOutline.set_shader_param("line_thickness", 10)
+	animation_state_machine = animationTree["parameters/playback"]
+	
+	var randomFrame = rand_range(0, 0.5)
+	animationTree.advance(randomFrame)
+
 
 func outline(show = false):
 	if show:
@@ -13,3 +20,8 @@ func outline(show = false):
 	else:
 		z_index = 0
 		material = null
+
+
+func animation(animation: String):
+	animation_state_machine.travel(animation)
+
