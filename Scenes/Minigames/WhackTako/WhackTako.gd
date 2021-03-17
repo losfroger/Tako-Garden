577e2d03dcc445bf90e2c8d78e2026310d80a03e
Emilio Puga <ea.pugaavalos@ugto.mx>
2021-03-17 17:10:07 -0600

    Added score and countdown timer to the main screen, also it adds or substracts to the score if a tako gets bonked or gets away

--- Scenes/Minigames/WhackTako/WhackTako.gd
+++ Scenes/Minigames/WhackTako/WhackTako.gd
@@ -1,8 +1,23 @@
 extends Node2D
 
+onready var takoContainer = $Takos
+onready var scoreLabel = $Control/VBoxContainer/Score
+
 func _ready() -> void:
 	randomize()
+	for tako in takoContainer.get_children():
+		tako.connect("bonked", self, "add_score")
+		tako.connect("missed", self, "reduce_score")
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("r_click"):
 		$Takos/TakoWhack.animationPlayer.play("pop")
+
+		
+func add_score():
+	scoreLabel.addScore(100)
+
+
+func reduce_score():
+	scoreLabel.addScore(-25)
