8b241aa3208ec344013dbda1651619f83581ded6
Emilio Puga <ea.pugaavalos@ugto.mx>
2021-03-17 17:06:47 -0600

    Added signals bonked and missed to the tako, also made duplicate code into a function

--- Scenes/Minigames/WhackTako/TakoHole.gd
+++ Scenes/Minigames/WhackTako/TakoHole.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+signal bonked()
+signal missed()
+
 enum STATE {
 	UP,
 	DOWN,
@@ -18,35 +21,37 @@ func _ready() -> void:
 	randomize()
 	takoSprite.animationTree.active = false
 	animationPlayer.play("default")
-	timer.wait_time = rand_range(5,10)
+	timer.wait_time = rand_range(1,12)
 	timer.start()
 
 
 func _on_Hurtbox_area_entered(area: Area2D) -> void:
 	timer.stop()
 	print("Bonk!")
+	emit_signal("bonked")
 	emote.emote(emote.EMOTES.circle, 0.5)
 	hurtBox.disabled = true
 	bonked = true
+	downTako()
+
+func downTako():
 	animationPlayer.play("down")
 	state = STATE.DOWN
 	timer.wait_time = rand_range(5,10)
 	timer.start()
 	bonked = false
 
-
 func _on_Timer_timeout() -> void:
 	match state:
 		STATE.UP:
-			animationPlayer.play("down")
 			if not bonked:
 				emote.emote(emote.EMOTES.cross, 0.5)
-			bonked = false
-			state = STATE.DOWN
-			timer.wait_time = rand_range(5,10)
-			timer.start()
+				emit_signal("missed")
+			downTako()
 		STATE.DOWN:
 			animationPlayer.play("pop")
 			state = STATE.UP
 			timer.wait_time = rand_range(0.8, 1.5)
 			timer.start()
+
+
