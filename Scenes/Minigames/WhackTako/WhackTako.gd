477fea9c647bebe3b81468da08ed3ddb5a3d0447
Emilio Puga <ea.pugaavalos@ugto.mx>
2021-03-17 22:48:26 -0600

    Added todos, and changed the route of the score label

--- Scenes/Minigames/WhackTako/WhackTako.gd
+++ Scenes/Minigames/WhackTako/WhackTako.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 onready var takoContainer = $Takos
-onready var scoreLabel = $Control/VBoxContainer/Score
+onready var scoreLabel = $CanvasLayer/Control/VBoxContainer/Score
 
+# TODO: Add the event when the timer gets to zero
+# TODO: Change speed when gaining more points so the takos are
+# harder to hit
 func _ready() -> void:
 	randomize()
 	for tako in takoContainer.get_children():
@@ -14,7 +17,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("r_click"):
 		$Takos/TakoWhack.animationPlayer.play("pop")
 
-		
+
 func add_score():
 	scoreLabel.addScore(100)
 
