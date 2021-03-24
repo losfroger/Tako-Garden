e01a1253b6b71c7713fce92981c00645c86fe838
Emilio Puga <ea.pugaavalos@ugto.mx>
2021-03-23 22:42:27 -0600

    Added retry button

--- Scenes/Minigames/WhackTako/GameOverScreen.gd
+++ Scenes/Minigames/WhackTako/GameOverScreen.gd
@@ -4,3 +4,7 @@ onready var scoreLabel = $VBoxContainer/ScoreLabel
 
 func score(number:int) -> void:
 	scoreLabel.text = "Score: " + String(number)
+
+
+func _on_Retry_pressed() -> void:
+	get_tree().reload_current_scene()
