facb8e9b6b0f1227670d99b14d9ce68cff2cd64d
Emilio Puga <ea.pugaavalos@ugto.mx>
2021-04-05 17:09:20 -0500

    Solved minor warning in volume slider

--- Scenes/UI/Configuration/VolumeSlider.gd
+++ Scenes/UI/Configuration/VolumeSlider.gd
@@ -4,7 +4,7 @@ export var audio_bus_name := "Master"
 onready var _bus := AudioServer.get_bus_index(audio_bus_name)
 onready var soundTest = $soundTest
 
-var _ready = false
+var _readyVolume = false
 
 func _ready() -> void:
 	soundTest.bus = audio_bus_name
@@ -13,6 +13,6 @@ func _ready() -> void:
 
 func _on_VolumeSlider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(_bus, linear2db(value))
-	if _ready:
+	if _readyVolume:
 		soundTest.play()
-	_ready = true
+	_readyVolume = true
