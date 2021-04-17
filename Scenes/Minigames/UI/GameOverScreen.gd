f93f38554ecf4235a20cfeea0328d07e1074e00b
Emilio Puga <ea.pugaavalos@ugto.mx>
2021-04-17 15:10:21 -0500

    Game over screen now has a blurScreen node, and on the show function you can pass the score

--- Scenes/Minigames/UI/GameOverScreen.gd
+++ Scenes/Minigames/UI/GameOverScreen.gd
@@ -1,6 +1,14 @@
-extends VBoxContainer
+extends Control
 
 onready var scoreLabel = $VBoxContainer/ScoreLabel
+onready var blurScreen = $BlurScreen
+
+func show(score = 0):
+	get_tree().paused = true
+	visible = true
+	score(score)
+	blurScreen.show()
+
 
 func score(number:int) -> void:
 	scoreLabel.text = "Score: " + String(number)
